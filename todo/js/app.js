243d4755b0e6ae4a3aaba84fd8658803f94aa52f
eduardonk9999 <thiago.novak7@gmail.com>
2021-09-19 13:27:25 -0300

    Add, init script

--- todo/js/app.js
+++ todo/js/app.js
@@ -1,1 +1,31 @@
- // CODE EXPLAINED channel
+const clear = document.querySelector(".clear")
+const dateElement = document.getElementById("date")
+const list = document.getElementById("list")
+const input = document.getElementById("input")
+
+const CHECK = "fa-check-circle"
+const UNCHECK = "fa-circle-thin"
+const LINE_THROUGH = "lineThrough"
+
+const options = {
+  weekday: "long",
+  month: "short",
+  day: "numeric"
+}
+const today = new Date()
+
+dateElement.innerHTML = today.toLocaleDateString("pt-BR", options)
+
+function addTodo(toDo) {
+  const item = `
+    <li class="item">
+      <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+      <p class="text">${toDo}</p>
+      <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    </li>
+  `
+  const position = "beforeend"
+
+  list.insertAdjacentHTML(position, item)
+}
+
